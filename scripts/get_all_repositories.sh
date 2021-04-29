b9e282c7b47754d2b6881a79f87dfd610637964b
Pascal Bourdier <pascal.bourdier@mirakl.com>
2021-04-29 10:33:54 +0200

    follow shellcheck advices

--- scripts/get_all_repositories.sh
+++ scripts/get_all_repositories.sh
@@ -11,14 +11,14 @@ eval "$(jq -r '@sh "GCR_HOST_NAME=\(.gcr_host_name) GOOGLE_PROJECT_ID=\(.google_
 
 # construct a string that holds the content of a json list object, without opening and closing brackets `[]`
 # "{\"name\":\"gcr.io/my-project/nested/repository\"},{\"name\":\"gcr.io/my-project/another/nested/repository\"},"
-function get_gcr_repositories {
+function get_gcr_repositories() {
     local main_repository=$1
     local repositories=""
-    while read nested_repository; do
-        if [ $(gcloud container images list --repository $nested_repository --format json | jq length) -eq 0 ]; then
+    while read -r nested_repository; do
+        if [ "$(gcloud container images list --repository "$nested_repository" --format json | jq length)" -eq 0 ]; then
             repositories+="{\"name\":\"${nested_repository}\"},"
         else
-            get_gcr_repositories $nested_repository
+            get_gcr_repositories "$nested_repository"
         fi
     done < <(gcloud container images list --repository "${main_repository}" --format json | jq -r '.[].name')
     echo "${repositories}"
@@ -31,7 +31,7 @@ repo_names="$(get_gcr_repositories "${GCR_HOST_NAME}"/"${GOOGLE_PROJECT_ID}")"
 # "gcr.io/my-project/nested/repository",
 # "gcr.io/my-project/another/nested/repository"
 # ]
-REPOSITORIES=$(echo "["${repo_names%%,}"]" | jq '.|map(.name)')
+REPOSITORIES=$(echo "[${repo_names%%,}]" | jq '.|map(.name)')
 
 # Safely produce a JSON object containing the result value.
 # jq will ensure that the value is properly quoted
