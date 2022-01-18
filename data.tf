0be1bb84f8c31dd5b9b1e56974a5d954abca5de6
Patrick Lubach <plubach1994@gmail.com>
2022-01-18 13:13:03 +0100

    refactor: remove external

--- data.tf
+++ data.tf
@@ -1,21 +1,2 @@
 # get project details
 data "google_project" "this" {}
-
-# get all repositories of a given project
-data "external" "this" {
-  for_each = {
-    for repo in local.project_all_repositories : repo.google_project_id => repo
-  }
-
-  program = ["${path.module}/scripts/get_all_repositories.sh"]
-  query = {
-    gcr_host_name     = each.value.gcr_host_name
-    google_project_id = each.value.google_project_id
-  }
-}
-
-data "google_storage_bucket" "bucket" {
-  for_each = toset(local.buckets)
-
-  name = each.value
-}
