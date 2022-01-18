9eb0fded8425455e60b9d25c5f70f75cc704ccd7
Anouar Chattouna <9251649+anouarchattouna@users.noreply.github.com>
2022-01-18 17:12:26 +0100

    Revert "add support for gcr buckets with uniform_bucket_level_access = true (#30)" (#31)
    This reverts commit b882a2179339e93d2177ad396eb0cf2c46870f6b.

--- data.tf
+++ data.tf
@@ -1,9 +1,2 @@
 # get project details
 data "google_project" "this" {}
-
-data "google_storage_bucket" "bucket" {
-  for_each = toset(local.buckets)
-
-  name = each.value
-}
-
--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.88.0"
+      version = ">= 4.1.0"
     }
   }
 }
