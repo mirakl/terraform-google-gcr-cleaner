d0469761e323107622634a3fcf5a2f8b22edfbd0
Antoine Rouaze <antoine.rouaze@gmail.com>
2022-08-09 12:40:05 -0400

    feat: Change the release process (#60)
    Use the new release-please-action GitHub action to release the Terraform
    module. This new way to release the module will open a release pull
    request that will append commits from main until we're ready to create
    a new release. The new way avoids manual release from the Makefile.
    
    Also, the release process now depends on Conventional Commits where this
    convention frames the way the commit message should be written. The
    messages must now contain a prefix that specifies the type of the
    changes (fix, feature, etc...). The GitHub action will use this prefix
    to sort the commit in the changelog. It will also use it to infer the
    next version number. For example, if the release contains only fixes,
    the new version will be a patch (ex: v1.0.0 -> v1.0.1).
    
    Because the repository already has a tag and release, we have to include
    some bootstrap configuration otherwise release-please will use 0.1.0 as
    the next release instead of 1.3.1 or 1.4.0.
    
    To make sure commit authors respect the new convention, this commit
    introduces a new pre-commit hook that enforces it.

--- locals.tf
+++ locals.tf
@@ -42,7 +42,7 @@ locals {
       tag_filter_all            = repo.parameters != null ? (repo.parameters.tag_filter_all != null ? repo.parameters.tag_filter_all : "") : ""
       recursive                 = true
       dry_run                   = repo.parameters != null ? (repo.parameters.dry_run != null ? repo.parameters.dry_run : false) : false
-      filter                    = repo.parameters != null ? "grace-${repo.parameters.grace != null ? repo.parameters.grace : "0"}-keep-${repo.parameters.keep != null ? repo.parameters.keep : "0"}-tag_filter-${repo.parameters.tag_filter != null ? repo.parameters.tag_filter : "no"}-tag_filter_any-${repo.parameters.tag_filter_any != null ? repo.parameters.tag_filter_any : "no"}-tag_filter_any-${repo.parameters.tag_filter_any != null ? repo.parameters.tag_filter_any : "no"}${repo.parameters.dry_run != null ? "${repo.parameters.dry_run ? "-dry_run" : ""}" : ""}" : "delete-all-untagged-images-recursive"
+      filter                    = repo.parameters != null ? "grace-${repo.parameters.grace != null ? repo.parameters.grace : "0"}-keep-${repo.parameters.keep != null ? repo.parameters.keep : "0"}-tag_filter-${repo.parameters.tag_filter != null ? repo.parameters.tag_filter : "no"}-tag_filter_any-${repo.parameters.tag_filter_any != null ? repo.parameters.tag_filter_any : "no"}-tag_filter_any-${repo.parameters.tag_filter_any != null ? repo.parameters.tag_filter_any : "no"}${repo.parameters.dry_run != null ? (repo.parameters.dry_run ? "-dry_run" : "") : ""}" : "delete-all-untagged-images-recursive"
       scheduler_job_name        = repo.parameters != null ? repo.parameters.scheduler_job_name : null
       scheduler_job_description = repo.parameters != null ? repo.parameters.scheduler_job_description : null
     } if repo.clean_all == true
@@ -62,7 +62,7 @@ locals {
           tag_filter_all            = repo.tag_filter_all != null ? repo.tag_filter_all : ""
           recursive                 = repo.recursive != null ? repo.recursive : false
           dry_run                   = repo.dry_run != null ? repo.dry_run : false
-          filter                    = "grace-${repo.grace != null ? repo.grace : "0"}-keep-${repo.keep != null ? repo.keep : "0"}-tag_filter-${repo.tag_filter != null ? repo.tag_filter : "no"}-tag_filter_any-${repo.tag_filter_any != null ? repo.tag_filter_any : "no"}-tag_filter_all-${repo.tag_filter_all != null ? repo.tag_filter_all : "no"}-recursive-${repo.recursive != null ? repo.recursive : false}${repo.dry_run != null ? "${repo.dry_run ? "-dry_run" : ""}" : ""}"
+          filter                    = "grace-${repo.grace != null ? repo.grace : "0"}-keep-${repo.keep != null ? repo.keep : "0"}-tag_filter-${repo.tag_filter != null ? repo.tag_filter : "no"}-tag_filter_any-${repo.tag_filter_any != null ? repo.tag_filter_any : "no"}-tag_filter_all-${repo.tag_filter_all != null ? repo.tag_filter_all : "no"}-recursive-${repo.recursive != null ? repo.recursive : false}${repo.dry_run != null ? (repo.dry_run ? "-dry_run" : "") : ""}"
           scheduler_job_name        = repo.scheduler_job_name
           scheduler_job_description = repo.scheduler_job_description
         }
