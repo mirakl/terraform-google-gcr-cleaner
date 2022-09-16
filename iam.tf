04cb5ada2af6da74b51f7e81413beadf4580b541
Paweł Rosada <pawelrosada@gmail.com>
2022-09-16 15:50:57 +0200

    GAR - Terraform errors with same repo name but different projects bug

--- iam.tf
+++ iam.tf
@@ -41,11 +41,11 @@ resource "google_project_iam_member" "this" {
 # and delete container images.
 resource "google_artifact_registry_repository_iam_member" "this" {
   for_each = {
-    for repo in var.gar_repositories : repo.name => repo...
+    for repo in var.gar_repositories : "${repo.name}_${repo.project_id}_${repo.region}" => repo...
   }
 
-  project    = each.value.project_id != null ? each.value.project_id : local.google_project_id
-  location   = each.value.region
+  project    = distinct(each.value).project_id != null ? distinct(each.value).project_id : local.google_project_id
+  location   = distinct(each.value).region
   repository = "projects/${each.value.project_id != null ? each.value.project_id : local.google_project_id}/locations/${each.value.region}/repositories/${each.value.registry_name}"
   role       = "roles/artifactregistry.repoAdmin"
   member     = "serviceAccount:${google_service_account.cleaner.email}"
